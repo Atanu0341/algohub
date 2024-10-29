98041cb3e8ac9b5fdf00cadbeb147f2af2b9724f
Atanu Majumder <atanumajumder2004@gmail.com>
2024-10-29 16:39:14 +0000

    added solution and topics

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -1,6 +1,30 @@
 export const dsaTopics = [
     {
         id: 1,
+        name: "Basics",
+        description: "Fundamental programming concepts including simple logic and conditionals",
+        questions: [
+            {
+                id: 1,
+                title: "Leap Year",
+                difficulty: "Basic",
+                platform: "GeeksforGeeks",
+                link: "https://www.geeksforgeeks.org/problems/leap-year0943/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/gfg/basic-maths/leap-year/solution.cpp"
+            },
+            {
+                id: 2,
+                title: "Trailing zeroes in factorial",
+                difficulty: "Basic",
+                platform: "GeeksforGeeks",
+                link: "https://www.geeksforgeeks.org/problems/trailing-zeroes-in-factorial5134/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/gfg/basic-maths/Trailing-zeroes-in-factorial/solution.cpp"
+            },
+            
+        ],
+    },
+    {
+        id: 2,
         name: "Arrays",
         description: "Basic and advanced array manipulation techniques",
         questions: [
