891bcc50b03b8eac3dea43b089a1cb999fc9660b
Atanu Majumder <atanumajumder2004@gmail.com>
2024-10-30 06:17:41 +0000

    added more topics

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -20,6 +20,86 @@ export const dsaTopics = [
                 link: "https://www.geeksforgeeks.org/problems/trailing-zeroes-in-factorial5134/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab",
                 solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/gfg/basic-maths/Trailing-zeroes-in-factorial/solution.cpp"
             },
+            {
+                id: 3,
+                title: "Add Digits",
+                difficulty: "Easy",
+                platform: "Leetcode",
+                link: "https://leetcode.com/problems/add-digits/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/leetcode/basic-maths/7-reverse-integer/solution.cpp"
+            },
+            {
+                id: 4,
+                title: "Palindrome Number",
+                difficulty: "Easy",
+                platform: "Leetcode",
+                link: "https://leetcode.com/problems/palindrome-number/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/leetcode/basic-maths/9-palindrome-number/solution.cpp"
+            },
+            {
+                id: 5,
+                title: "Nim Game",
+                difficulty: "Easy",
+                platform: "Leetcode",
+                link: "https://leetcode.com/problems/nim-game/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/leetcode/basic-maths/292-nim-game/solution.cpp"
+            },
+            {
+                id: 6,
+                title: "Add Digits",
+                difficulty: "Easy",
+                platform: "Leetcode",
+                link: "https://leetcode.com/problems/add-digits/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/leetcode/basic-maths/258-add-digits/solution.cpp"
+            },
+            {
+                id: 7,
+                title: "Power of Two",
+                difficulty: "Easy",
+                platform: "Leetcode",
+                link: "https://leetcode.com/problems/power-of-two/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/leetcode/basic-maths/231-power-of-two/solution.cpp"
+            },
+            {
+                id: 8,
+                title: "Number Complement",
+                difficulty: "Easy",
+                platform: "Leetcode",
+                link: "https://leetcode.com/problems/number-complement/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/leetcode/basic-maths/476-and-1009-complement-of-base-10-integer/solution-476.cpp"
+            },
+            {
+                id: 9,
+                title: "Complement of Base 10 Integer",
+                difficulty: "Easy",
+                platform: "Leetcode",
+                link: "https://leetcode.com/problems/complement-of-base-10-integer/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/leetcode/basic-maths/476-and-1009-complement-of-base-10-integer/solution-1009.cpp"
+            },
+            {
+                id: 10,
+                title: "Is Rectangle?",
+                difficulty: "Easy",
+                platform: "InterviewBit",
+                link: "https://www.interviewbit.com/problems/is-rectangle/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/interviewbit/basicMaths/valid-rectangle/solution.cpp"
+            },
+            {
+                id: 11,
+                title: "Distribute in Circle!",
+                difficulty: "Easy",
+                platform: "InterviewBit",
+                link: "https://www.interviewbit.com/problems/distribute-in-circle/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/interviewbit/basicMaths/distribute-in-circle/solution.cpp"
+            },
+            {
+                id: 12,
+                title: "Total Moves For Bishop!",
+                difficulty: "Easy",
+                platform: "InterviewBit",
+                link: "https://www.interviewbit.com/problems/total-moves-for-bishop/",
+                solution_link: "https://github.com/Atanu0341/0-to-dsa/blob/main/interviewbit/basicMaths/Total-Moves-For-Bishop!/solution.cpp"
+            },
             
         ],
     },
